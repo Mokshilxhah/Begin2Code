bf55c5198b5aad3e314b473ace44435dd425cc7d
Mokshil Shah <mokshilshah000@gmail.com>
2026-02-17 19:56:00 +0530

    Update notes_data.py

--- data/notes_data.py
+++ data/notes_data.py
@@ -2,141 +2,141 @@ NOTES = [
     {
         "name": "C++",
         "image": "Notes/CC++.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/C++FN.pdf",
+        "short": "Short/C++SN.pdf",
+        "mind": "Mind/C++MM.pdf"
     },
     {
         "name": "Java",
         "image": "Notes/Java.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/JavaFN.pdf",
+        "short": "Short/JavaSN.pdf",
+        "mind": "Mind/JavaMM.pdf"
     },
     {
         "name": "Python",
         "image": "Notes/Python.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/PythonFN.pdf",
+        "short": "Short/PythonSN.pdf",
+        "mind": "Mind/PythonMM.pdf"
     },
     {
         "name": "JavaScript",
         "image": "Notes/Javascript.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/JsFN.pdf",
+        "short": "Short/JsSN.pdf",
+        "mind": "Mind/JsMM.pdf"
     },
     {
         "name": "HTML",
         "image": "Notes/Html.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/HtmlFN.pdf",
+        "short": "Short/HtmlSN.pdf",
+        "mind": "Mind/HtmlMM.pdf"
     },
     {
         "name": "Css",
         "image": "Notes/Css.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/CssFN.pdf",
+        "short": "Short/CssSN.pdf",
+        "mind": "Mind/CssMM.pdf"
     },
     {
         "name": "React",
         "image": "Notes/React.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/ReactFN.pdf",
+        "short": "Short/ReactSN.pdf",
+        "mind": "Mind/ReactMM.pdf"
     },
     {
         "name": "Angular",
         "image": "Notes/Angular.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/AngularFN.pdf",
+        "short": "Short/AngularSN.pdf",
+        "mind": "Mind/AngularMM.pdf"
     },
     {
         "name": "Node.js",
         "image": "Notes/Node.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/NodeFN.pdf",
+        "short": "Short/NodeSN.pdf",
+        "mind": "Mind/NodeMM.pdf"
     },
     {
         "name": "Express.js",
         "image": "Notes/Express.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/ExpressFN.pdf",
+        "short": "Short/ExpressSN.pdf",
+        "mind": "Mind/ExpressMM.pdf"
     },
     {
         "name": "TypeScript",
         "image": "Notes/Typescript.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/TypeScriptFN.pdf",
+        "short": "Short/TypeScriptSN.pdf",
+        "mind": "Mind/TypeScriptMM.pdf"
     },
     {
         "name": "Value.js",
         "image": "Notes/Value.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/ValueFN.pdf",
+        "short": "Short/ValueSN.pdf",
+        "mind": "Mind/ValueMM.pdf"
     },
     {
         "name": "Spring Boot",
         "image": "Notes/Spring.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/SpringFN.pdf",
+        "short": "Short/SpringSN.pdf",
+        "mind": "Mind/SpringMM.pdf"
     },
     {
         "name": "Bootstrap",
         "image": "Notes/Bootstrap.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/BootstrapFN.pdf",
+        "short": "Short/BootstrapSN.pdf",
+        "mind": "Mind/BootstrapMM.pdf"
     },
     {
         "name": "Django",
         "image": "Notes/Django.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/DjangoFN.pdf",
+        "short": "Short/DjangoSN.pdf",
+        "mind": "Mind/DjangoMM.pdf"
     },
     {
         "name": "Flask",
         "image": "Notes/Flask.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/FlaskFN.pdf",
+        "short": "Short/FlaskSN.pdf",
+        "mind": "Mind/FlaskMM.pdf"
     },
     {
         "name": "MySQL",
         "image": "Notes/Mysql.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/MysqlFN.pdf",
+        "short": "Short/MysqlSN.pdf",
+        "mind": "Mind/MysqlMM.pdf"
     },
     {
         "name": "MongoDB",
         "image": "Notes/Mongo.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/MongoFN.pdf",
+        "short": "Short/MongoSN.pdf",
+        "mind": "Mind/MongoMM.pdf"
     },
     {
         "name": "Firebase",
         "image": "Notes/Firebase.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/FirebaseFN.pdf",
+        "short": "Short/FirebaseSN.pdf",
+        "mind": "Mind/FirebaseMM.pdf"
     },
     {
         "name": "Redis",
         "image": "Notes/Redis.png",
-        "full": "Notes_pdf/.pdf",
-        "short": "Notes_pdf/.pdf",
-        "mind": "Notes_pdf/.pdf"
+        "full": "Full/RedisFN.pdf",
+        "short": "Short/RedisSN.pdf",
+        "mind": "Mind/RedisMM.pdf"
     }
 ]
